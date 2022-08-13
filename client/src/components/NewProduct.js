dcaba1e3464b328d6524140c04ea966094bc51fb
arbreshazeqiri <arbresha.zeqiri@student.uni-pr.edu>
2022-08-13 12:56:08 +0200

    File storing finished, file uploading left

--- client/src/components/NewProduct.js
+++ client/src/components/NewProduct.js
@@ -6,172 +6,104 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Swal from 'sweetalert2';
 
 const NewProduct = ({ isLoggedin, setIsLoggedin }) => {
-  // const [user, setUser] = useState(null);
-  // const [title, setTitle] = useState('');
-  // const [description, setDescription] = useState('');
-  // const [location, setLocation] = useState('');
-  // const [category, setCategory] = useState('');
-  // const [condition, setCondition] = useState('');
-  // const [delivery, setDelivery] = useState('');
-  // const [price, setPrice] = useState(0);
-  // const [name, setName] = useState("");
-  // const [img, setImg] = useState("");
-  // const [errors, setErrors] = useState({});
-  // const [selectedFile, setSelectedFile] = useState("");
-  // const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
+  const [category, setCategory] = useState('');
+  const [condition, setCondition] = useState('');
+  const [delivery, setDelivery] = useState('');
+  const [price, setPrice] = useState('');
+  const [names, setNames] = useState([]);
+  const [nameOne, setNameOne] = useState('');
+  const [nameTwo, setNameTwo] = useState('');
+  const [nameThree, setNameThree] = useState('');
+  const [nameFour, setNameFour] = useState('');
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
   let [images, setImages] = useState([]);
-  const maxNumber = 69;
+  const maxNumber = 4;
+  const fd = new FormData();
 
   const onChange = (imageList, addUpdateIndex) => {
-    // data for submit
-    console.log(imageList, addUpdateIndex);
-
     setImages(imageList);
-  };
-  const uploadimages = () => {
-    for (var a = 0; a < images.length; a++) {
-      const fd = new FormData();
-      //console.log(images[a])
-      fd.append('image', images[a]['file']);
-
-      //Post Request to Nodejs API Route
-      axios.post('http://localhost:8000/upload', fd
-      ).then(res => {
-        //Success Message in Sweetalert modal
-        Swal.fire({
-          title: 'Images hava been uploaded successfully.',
-          text: "Thanks",
-          type: 'success',
-
-        });
-
-
-      });
-    }
-
   }
 
-  // useEffect(() => {
-  //   axios
-  //     .get('http://localhost:8000/api/current-user', { withCredentials: true })
-  //     .then((res) => {
-  //       setUser(res.data);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, [isLoggedin,selectedFile, name]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/api/current-user', { withCredentials: true })
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [isLoggedin]);
 
-  // const submitForm = (e) => {
-  // e.preventDefault();
-  // const formData = new FormData();
-  // formData.append("name", name);
-  // formData.append("file", selectedFile);
+  const fillImgPaths = () => {
+    for (var a = 0; a < images.length; a++) {
+      fd.append('image', images[a]['file']);
+      names.push(images[a]['file']['name']);
+    }
+    console.log(names);
+    if (images.length === 1) {
+      setNameOne(names[0].toString());
+    }
+    if (images.length === 2) {
+      setNameOne(names[0].toString());
+      setNameTwo(names[1].toString());
+    }
+    if (images.length === 3) {
+      console.log(names[0]);
+      setNameTwo(names[1].toString());
+      console.log(nameTwo);
+      setNameThree(names[2].toString());
+      console.log(nameThree);
+    }
+    if (images.length === 4) {
+      setNameOne(names[0].toString());
+      setNameTwo(names[1].toString());
+      setNameThree(names[2].toString());
+      setNameFour(names[3].toString());
+    }
+  }
 
-  // axios.post('http://localhost:8000/upload', formData
-  // ).then(res => {
-  //   //Success Message in Sweetalert modal
-  //   Swal.fire({
-  //     title: 'Images hava been uploaded successfully.',
-  //     text: "Thanks",
-  //     type: 'success',
-
-  //   });
-  // });
-
-  // axios
-  //   .post(
-  //     'http://localhost:8000/api/products',
-  //     {
-  //       // title,
-  //       // description,
-  //       // location,
-  //       // category,
-  //       // condition,
-  //       // delivery,
-  //       // price,
-  //       name,
-  //     },
-  //     { withCredentials: true },
-  //   )
-  //   .then((res) => {
-  //     console.log(res.data);
-  //     // navigate('/profile/' + user.username);
-  //   })
-  //   .catch((err) => setErrors(err.response.data.errors));
-  //};
-
-  // return (
-  // <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-  //   <label>Title</label>
-  //   <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-  //   {errors.title && <span className="text-danger">{errors.title.message}</span>}
-  //   <label>Description</label>
-  //   <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
-  //   {errors.description && <span className="text-danger">{errors.description.message}</span>}
-  //   <label>Location</label>
-  //   <select value={location} name="location" onChange={(e) => setLocation(e.target.value)}>
-  //     <option>Select a Location</option>
-  //     <option value="Prishtinë">Prishtinë</option>
-  //     <option value="Mitrovicë">Mitrovicë</option>
-  //     <option value="Ferizaj">Ferizaj</option>
-  //     <option value="Pejë">Pejë</option>
-  //     <option value="Gjakovë">Gjakovë</option>
-  //     <option value="Gjilan">Gjilan</option>
-  //     <option value="Prizren">Prizren</option>
-  //     <option value="Tiranë">Tiranë</option>
-  //     <option value="Shkup">Shkup</option>
-  //   </select>
-  //   {errors.location && <span className="text-danger">{errors.location.message}</span>}
-  //   <label>Category</label>
-  //   <select value={category} name="category" onChange={(e) => setCategory(e.target.value)}>
-  //     <option>Select a category</option>
-  //     <option value="tops">Tops</option>
-  //     <option value="bottoms">Bottoms</option>
-  //     <option value="skirts">Skirts</option>
-  //     <option value="jeans">Jeans</option>
-  //     <option value="bags">Bags</option>
-  //     <option value="glasses">Glasses</option>
-  //     <option value="jewellery">Jewellery</option>
-  //     <option value="shoes">Shoes</option>
-  //     <option value="heels">Heels</option>
-  //     <option value="sneakers">Sneakers</option>
-  //   </select>
-  //   {errors.category && <span className="text-danger">{errors.category.message}</span>}
-  //   <label>Condition</label>
-  //   <select value={condition} name="condition" onChange={(e) => setCondition(e.target.value)}>
-  //     <option>Select product condition description</option>
-  //     <option value="new">New</option>
-  //     <option value="used">Used</option>
-  //     <option value="fairly used">Fairly used</option>
-  //   </select>
-  //   {errors.condition && <span className="text-danger">{errors.condition.message}</span>}
-  //   <label>Delivery</label>
-  //   <select value={delivery} name="delivery" onChange={(e) => setDelivery(e.target.value)}>
-  //     <option>Select delivery method</option>
-  //     <option value="my own shipping">My own Shipping</option>
-  //     <option value="pick up">Pick up</option>
-  //   </select>
-  //   {errors.delivery && <span className="text-danger">{errors.delivery.message}</span>}
-  //   <label>Price</label>
-  //   <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
-  //   {errors.price && <span className="text-danger">{errors.price.message}</span>}
-  {/* <input
-        type="file"
-        value={""}
-        onChange={(e) => { setSelectedFile(e.target.files[0]); setImg(URL.createObjectURL(e.target.files[0])); setName(e.target.files[0].name) }} />
-      <img src={img} alt="" /> */}
-  {/* <button onClick={submitForm}>Submit</button> */ }
-
-  {/*</form> */ }
-  //   );
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fillImgPaths();
+    console.log(nameOne);
+    axios
+      .post(
+        'http://localhost:8000/api/products',
+        {
+          title,
+          description,
+          location,
+          category,
+          condition,
+          delivery,
+          price,
+          nameOne,
+          nameTwo,
+          nameThree,
+          nameFour
+        },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        console.log(res.data);
+        axios.post('http://localhost:8000/upload', fd
+        ).then(res => {
+        })
+        navigate('/profile/' + user.username);
+      })
+      .catch((err) => {
+        setErrors(err.response.data.errors);
+        console.log(err.response.data.errors);
+      });
+  };
 
   return (
     <div className="App">
-
-      <h1>Therichpost.com</h1>
-
-
-      <div>
+      <div className="uploaded-images">
         <ImageUploading
           multiple
           value={images}
@@ -201,20 +133,78 @@ const NewProduct = ({ isLoggedin, setIsLoggedin }) => {
 
                 <button className="btn btn-danger" onClick={onImageRemoveAll}>Remove all images</button>
               </div>
-              {imageList.map((image, index) => (
-                <div key={index} className="image-item mt-5 mb-5 mr-5">
-                  <img src={image['data_url']} />
-                  <div className="image-item__btn-wrapper">
-                    <button className="btn btn-primary" onClick={() => onImageUpdate(index)}>Update</button>
-                    <button className="btn btn-danger" onClick={() => onImageRemove(index)}>Remove</button>
+              <div className="row2">
+                {imageList.map((image, index) => (
+                  <div key={index} className="image-item mt-5 mb-5 mr-5">
+                    <img src={image['data_url']} />
+                    <div className="image-item__btn-wrapper">
+                      <button className="btn btn-primary" onClick={() => onImageUpdate(index)}>Update</button>
+                      <button className="btn btn-danger" onClick={() => onImageRemove(index)}>Remove</button>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           )}
         </ImageUploading>
+        {errors.nameOne && <span className="text-danger">{errors.nameOne.message}</span>}
       </div>
-      <button className="btn btn-primary" onClick={() => uploadimages()}>Submit Images</button>
+      <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        {errors.title && <span className="text-danger">{errors.title.message}</span>}
+        <label>Description</label>
+        <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+        {errors.description && <span className="text-danger">{errors.description.message}</span>}
+        <label>Location</label>
+        <select value={location} name="location" onChange={(e) => setLocation(e.target.value)}>
+          <option>Select a Location</option>
+          <option value="Prishtinë">Prishtinë</option>
+          <option value="Mitrovicë">Mitrovicë</option>
+          <option value="Ferizaj">Ferizaj</option>
+          <option value="Pejë">Pejë</option>
+          <option value="Gjakovë">Gjakovë</option>
+          <option value="Gjilan">Gjilan</option>
+          <option value="Prizren">Prizren</option>
+          <option value="Tiranë">Tiranë</option>
+          <option value="Shkup">Shkup</option>
+        </select>
+        {errors.location && <span className="text-danger">{errors.location.message}</span>}
+        <label>Category</label>
+        <select value={category} name="category" onChange={(e) => setCategory(e.target.value)}>
+          <option>Select a category</option>
+          <option value="tops">Tops</option>
+          <option value="bottoms">Bottoms</option>
+          <option value="skirts">Skirts</option>
+          <option value="jeans">Jeans</option>
+          <option value="bags">Bags</option>
+          <option value="glasses">Glasses</option>
+          <option value="jewellery">Jewellery</option>
+          <option value="shoes">Shoes</option>
+          <option value="heels">Heels</option>
+          <option value="sneakers">Sneakers</option>
+        </select>
+        {errors.category && <span className="text-danger">{errors.category.message}</span>}
+        <label>Condition</label>
+        <select value={condition} name="condition" onChange={(e) => setCondition(e.target.value)}>
+          <option>Select product condition description</option>
+          <option value="new">New</option>
+          <option value="used">Used</option>
+          <option value="fairly used">Fairly used</option>
+        </select>
+        {errors.condition && <span className="text-danger">{errors.condition.message}</span>}
+        <label>Delivery</label>
+        <select value={delivery} name="delivery" onChange={(e) => setDelivery(e.target.value)}>
+          <option>Select delivery method</option>
+          <option value="my own shipping">My own Shipping</option>
+          <option value="pick up">Pick up</option>
+        </select>
+        {errors.delivery && <span className="text-danger">{errors.delivery.message}</span>}
+        <label>Price</label>
+        <input type="number" value={price} onChange={(e) => setPrice(e.target.value.toString())} />
+        {errors.price && <span className="text-danger">{errors.price.message}</span>}
+        <button id="styled-button-one" style={{ width: "300px", margin: "20px" }}>Submit</button>
+      </form>
     </div>
   );
 };
